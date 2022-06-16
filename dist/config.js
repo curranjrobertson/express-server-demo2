36b53f6e1f55284d3c4c7e2575897b92a751826d
curranjrobertson <100748551+curranjrobertson@users.noreply.github.com>
2022-06-16 13:49:40 +0200

    local emulator edit

--- dist/config.js
+++ dist/config.js
@@ -1,7 +1,13 @@
 import admin from 'firebase-admin';
+const useEmulator = true;
+if (useEmulator) {
+    process.env['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080';
+}
 const adminInit = () => admin.initializeApp({
     // to do: move credential data to .env file
     credential: admin.credential.cert('/Users/curranrobertson/projects/express-server-demo/src/fb-config.json'),
-    databaseURL: 'https://express-server-demo-default-rtdb.europe-west1.firebasedatabase.app'
+    databaseURL: 'http://localhost:8080/?ns=express-server-demo'
 });
 export { admin, adminInit };
+// detabaseURL for using cloud storage
+// https://express-server-demo-default-rtdb.europe-west1.firebasedatabase.app'
